73dbc5798c2b8a00382dadc5c676d6f95f1872ca
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-09-27 18:09:15 -0300

    feat: add links in banner

--- components/embelleze/Footer/EmbellezeFooter.tsx
+++ components/embelleze/Footer/EmbellezeFooter.tsx
@@ -9,12 +9,13 @@ export interface Props {
   imagens?: Array<{
     image: LiveImage;
     description?: string;
+    link: string;
   }>;
 }
 
 export default function EmbellezeFooter({ text, imagens, logo }: Props) {
   return (
-    <div class="bg-primary flex items-center flex-col">
+    <div class="bg-primary flex items-center flex-col py-8 px-4">
       {logo?.image && (
         <img src={logo?.image} alt={logo?.description} width="250px" />
       )}
@@ -23,9 +24,10 @@ export default function EmbellezeFooter({ text, imagens, logo }: Props) {
           {text}
         </p>
       ))}
-      <ul class="flex justify-evenly flex-wrap">
+      <ul class="flex justify-evenly flex-wrap gap-4">
         {imagens?.map((item) => (
-          <li class="mr-5 mt-3">
+          <li>
+            <a href={item.link} target="_blank" rel="noopener noreferrer"></a>
             <img src={item.image} alt={item.description} max-height="120px" />
           </li>
         ))}
